2b6cd865b125b3136fc51fbbc61513350d766295
YureAnjos <yuretutorias@gmail.com>
2024-08-17 11:15:30 -0300

    add link to header title

--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import LoginDialog from "./login-dialog";
 import RegisterDialog from "./register-dialog";
 import { Input } from "./ui/input";
@@ -6,7 +7,9 @@ function Header() {
   return (
     <header className="flex justify-center border-b border-secondary">
       <nav className="flex justify-between w-full max-w-5xl p-4 items-center">
-        <h1 className="font-bold uppercase text-xl">GL News</h1>
+        <Link to="/">
+          <h1 className="font-bold uppercase text-xl">GL News</h1>
+        </Link>
         <Input type="text" placeholder="Search..." className="w-auto" />
         <div className="flex gap-4">
           <RegisterDialog />
